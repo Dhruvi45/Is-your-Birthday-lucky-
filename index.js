29ac0e317d0015afd4e2f7dd4090e56db62b946a
Dhruvi45 <dhruvijoshi45@gmail.com>
2022-11-29 22:17:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const dateOfBirth = document.querySelector(".date-of-birth");
+const luckyNumber = document.querySelector(".lucky-number");
+const checkBtn = document.querySelector(".check-here");
+const outputBox = document.querySelector(".output-box");
+
+checkBtn.addEventListener('click',  checkBirthdateLuck );
+
+function checkBirthdateLuck(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(sum && dob){
+        compareValues(sum,luckyNumber.value);
+    }
+    
+    
+    else{
+        outputBox.innerText="Enter both fields"
+    }
+}
+
+function compareValues(sum, luckyNumber){
+    
+    if(sum % luckyNumber ===0){
+        outputBox.innerText="Your Birthdate is Lucky"
+    } else{
+        outputBox.innerText= "Your birthdate is not lucky"
+    }
+    
+}
+
+function calculateSum(dob){
+    dob= dob.replaceAll("-","");
+    let sum=0;
+    for(let index=0; index<dob.length;index++){
+        sum= sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
+
+
+
+
